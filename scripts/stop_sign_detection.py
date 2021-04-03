0c6e6243ee53b05477cf6f2520721aa6d1f24104
victoryfb <victoryfb@foxmail.com>
2021-04-02 21:40:25 -0400

    cleaned code

--- scripts/stop_sign_detection.py
+++ scripts/stop_sign_detection.py
@@ -11,7 +11,6 @@ import tensorflow as tf
 
 from yolov3.yolov3_tf2.models import YoloV3Tiny
 from yolov3.yolov3_tf2.dataset import transform_images
-from yolov3.yolov3_tf2.utils import draw_outputs
 
 
 # Get absolute base path for the folder containing the scripts
@@ -26,20 +25,18 @@ class StopSignDetection:
         self.image_sub = rospy.Subscriber(
             "/camera/rgb/image_raw", Image, self.camera_callback)
 
-        # Publisher which will publish to the topic 'camera/rgb/image_raw'
-        self.image_pub = rospy.Publisher('/camera/rgb/image_raw',
-                                         Image, queue_size=10)
-
         # Publisher which will publish to the topic '/cmd_vel'.
         self.detection_pub = rospy.Publisher('/stop_sign',
                                              Float32MultiArray, queue_size=10)
 
         # Init the detection result
         self.detection = Float32MultiArray()
+        self.detection.data = []
 
         # Init the publish rate
         self.rate = rospy.Rate(20)
 
+        # Init the YoloV3 model
         self.yolo = yolo
         self.class_names = class_names
 
@@ -56,35 +53,32 @@ class StopSignDetection:
 
         # Loop through the detections
         for i in range(nums[0]):
-            # Publish when the stop sign is detected
+            # Get the detected stop sign
             if 'stop sign' in self.class_names[int(classes[0][i])]:
                 self.detection.data = [
                     scores[0][i].numpy()] + boxes[0][i].numpy().tolist()
-
-                # Publish
-                self.detection_pub.publish(self.detection)
-                self.rate.sleep()
-
-                #######
-                # The below part should move to the integrated script
-                ######
-                # Draw the detection on the image
-                img = cv2.cvtColor(img_raw, cv2.COLOR_RGB2BGR)
-                wh = np.flip(img.shape[0:2])
-                x1y1 = tuple(
-                    (np.array(boxes[0][i][0:2]) * wh).astype(np.int32))
-                x2y2 = tuple(
-                    (np.array(boxes[0][i][2:4]) * wh).astype(np.int32))
-                img = cv2.rectangle(img, x1y1, x2y2, (255, 0, 0), 2)
-                img = cv2.putText(img, '{} {:.4f}'.format(
-                    self.class_names[int(classes[0][i])], scores[0][i]),
-                    x1y1, cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 0, 255), 2)
-                cv2.imshow("Detection", img)
-                cv2.waitKey(1)
-
                 break
             else:
                 continue
+
+        # Publish
+        self.detection_pub.publish(self.detection)
+        self.rate.sleep()
+
+        #######
+        # The below part should move to the integrated script
+        ######
+        # Draw the detection on the image
+        img = cv2.cvtColor(img_raw, cv2.COLOR_RGB2BGR)
+        wh = np.flip(img.shape[0:2])
+        x1y1 = tuple((np.array(boxes[0][i][0:2]) * wh).astype(np.int32))
+        x2y2 = tuple((np.array(boxes[0][i][2:4]) * wh).astype(np.int32))
+        img = cv2.rectangle(img, x1y1, x2y2, (255, 0, 0), 2)
+        img = cv2.putText(img, '{} {:.4f}'.format(self.class_names[int(
+            classes[0][i])], scores[0][i]), x1y1, cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 0, 255), 2)
+
+        cv2.imshow("Detection", img)
+        cv2.waitKey(1)
 
     def clean_up(self):
         cv2.destroyAllWindows()
