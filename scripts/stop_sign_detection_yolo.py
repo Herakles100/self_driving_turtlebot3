2a9acb395a03c6931750a84fdaa108ac0da72efa
Shengli <victoryxufb@gmail.com>
2021-04-11 22:30:09 -0400

    updated yolo based stop sign detection so that it can take the advantage of GPU

--- scripts/stop_sign_detection_yolo.py
+++ scripts/stop_sign_detection_yolo.py
@@ -139,4 +139,8 @@ def main():
 
 
 if __name__ == '__main__':
+    physical_devices = tf.config.experimental.list_physical_devices('GPU')
+    for physical_device in physical_devices:
+        tf.config.experimental.set_memory_growth(physical_device, True)
+
     main()
