7220e7537922a83e9357d0f98d5910f24a1ae8c5
Nitin Madhok <nitinma@g.clemson.edu>
2021-04-25 17:06:55 -0400

    Code Cleanup for move_turtlebot3.py

--- scripts/move_turtlebot3.py
+++ scripts/move_turtlebot3.py
@@ -1,20 +1,46 @@
 #!/usr/bin/env python3
+
+# Import libraries
 import rospy
 from geometry_msgs.msg import Twist
 
 
 class MoveTurtleBot3:
+    """
+    Move TurtleBot3 class for moving turtlebot3
+    """
+
     def __init__(self):
-        self.cmd_vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+        """
+        Init function for MoveTurtleBot3 class
+        """
+
+        self.cmd_vel_pub = rospy.Publisher(
+            '/cmd_vel', Twist, queue_size=10
+        )
+
         self.cmd_vel_subs = rospy.Subscriber(
-            '/cmd_vel', Twist, self.cmdvel_callback)
+            '/cmd_vel', Twist, self.cmdvel_callback
+        )
+
         self.last_cmdvel_command = Twist()
+
         self._cmdvel_pub_rate = rospy.Rate(10)
 
+
     def cmdvel_callback(self, msg):
+        """
+        Callback function called when data is published to the
+        '/cmd_vel' topic
+        """
         self.last_cmdvel_command = msg
 
+
     def compare_twist_commands(self, twist1, twist2):
+        """
+        Function to compare current Twist message with previous
+        Twist message
+        """
         LX = twist1.linear.x == twist2.linear.x
         LY = twist1.linear.y == twist2.linear.y
         LZ = twist1.linear.z == twist2.linear.z
@@ -22,22 +48,37 @@ class MoveTurtleBot3:
         AY = twist1.angular.y == twist2.angular.y
         AZ = twist1.angular.z == twist2.angular.z
         equal = LX and LY and LZ and AX and AY and AZ
+
         if not equal:
             rospy.logwarn(
-                "The Current Twist is not the same as the one sent, Resending")
+                "The Current Twist is not the same as the one sent,"
+                "Resending"
+            )
         return equal
 
+
     def move_robot(self, twist_object):
+        """
+        Function to move the robot by publishing data to the
+        '/cmd_vel' topic
+        """
         # We make this to avoid Topic loss, specially at the start
         current_equal_to_new = False
-        while (not (current_equal_to_new)):
+        while not current_equal_to_new:
             self.cmd_vel_pub.publish(twist_object)
             self._cmdvel_pub_rate.sleep()
-            current_equal_to_new = self.compare_twist_commands(twist1=self.last_cmdvel_command,
-                                                               twist2=twist_object)
+            current_equal_to_new = \
+                self.compare_twist_commands(
+                    twist1=self.last_cmdvel_command,
+                    twist2=twist_object
+                )
+
 
     def clean_class(self):
-        # Stop Robot
+        """
+        Function to clean class components
+        """
+        # Stop the Robot
         twist_object = Twist()
         twist_object.angular.z = 0.0
         self.move_robot(twist_object)
